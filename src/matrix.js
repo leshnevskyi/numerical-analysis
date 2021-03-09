cf05abe7ff75243d6ea3e19cf4edc91bbefd6cd4
InexLeshnevski <dvulyat.i@gmail.com>
2021-03-09 23:34:36 +0200

    Replace spaces with tabs

--- src/matrix.js
+++ src/matrix.js
@@ -1,36 +1,36 @@
 class Matrix {
-  constructor(matrix = [[]]) {
-    this.matrix = matrix;
-  }
+	constructor(matrix = [[]]) {
+		this.matrix = matrix;
+	}
 
-  replaceRow(rowIndex, newRow) {
-    let newMatrix = new Matrix(this.matrix);
+	replaceRow(rowIndex, newRow) {
+		let newMatrix = new Matrix(this.matrix);
 
-    newMatrix.matrix[rowIndex] = newRow;
+		newMatrix.matrix[rowIndex] = newRow;
 
-    return newMatrix;
-  }
+		return newMatrix;
+	}
 
-  replaceCol(colIndex, newCol) {
-    const newMatrix = new Matrix(this.matrix.map(([...row], rowIndex) => {
-      row[colIndex] = newCol[rowIndex];
+	replaceCol(colIndex, newCol) {
+		const newMatrix = new Matrix(this.matrix.map(([...row], rowIndex) => {
+			row[colIndex] = newCol[rowIndex];
 
-      return row;
-    }));
+			return row;
+		}));
 
-    return newMatrix;
-  }
+		return newMatrix;
+	}
 
-  getSubmatrix(rowIndex, colIndex) {
-    const i = rowIndex;
-    const j = colIndex;
+	getSubmatrix(rowIndex, colIndex) {
+		const i = rowIndex;
+		const j = colIndex;
 
-    const submatrix = this.matrix
-      .filter((_, rowIndex) => rowIndex !== i)
-      .map(row => row.filter((_, colIndex) => colIndex !== j));
+		const submatrix = this.matrix
+			.filter((_, rowIndex) => rowIndex !== i)
+			.map(row => row.filter((_, colIndex) => colIndex !== j));
 
-    return new Matrix(submatrix);
-  }
+		return new Matrix(submatrix);
+	}
 
 	getMinor(rowIndex, colIndex) {
 		const submatrix = this.getSubmatrix(rowIndex, colIndex);
@@ -39,99 +39,99 @@ class Matrix {
 		return determinant;
 	}
 
-  get size() {
-    const size = {
-      rows: this.matrix.length,
-      cols: this.matrix[0].length,
-    }
+	get size() {
+		const size = {
+			rows: this.matrix.length,
+			cols: this.matrix[0].length,
+		}
 
-    return size;
-  }
+		return size;
+	}
 
-  get determinant() {
-    if (this.size.rows !== this.size.cols) {
-      console.warn(`Can't find the determinant of a non-square matrix`);
+	get determinant() {
+		if (this.size.rows !== this.size.cols) {
+			console.warn(`Can't find the determinant of a non-square matrix`);
 
-      return;
-    }
+			return;
+		}
 
-    if (this.size.rows === 1) return this.matrix[0][0];
+		if (this.size.rows === 1) return this.matrix[0][0];
 
-    const rowIndex = 0;
+		const rowIndex = 0;
 
-    const determinant = this.matrix[rowIndex].reduce((acc, el, colIndex) => {
-      const submatrix = this.getSubmatrix(rowIndex, colIndex);
+		const determinant = this.matrix[rowIndex].reduce((acc, el, colIndex) => {
+			const submatrix = this.getSubmatrix(rowIndex, colIndex);
 
-      return acc += (-1) ** colIndex * el * submatrix.determinant;
-    }, 0);
+			return acc += (-1) ** colIndex * el * submatrix.determinant;
+		}, 0);
 
-    return determinant; 
-  }
+		return determinant; 
+	}
 }
 
 class LinearSystem {
-  static methods = {
-    cramer: Symbol('cramer'),
-    matrix: Symbol('matrix'),
-  }
+	static methods = {
+		cramer: Symbol('cramer'),
+		matrix: Symbol('matrix'),
+	}
 
-  #methods = {
-    cramer: () => {
-      const solution = {};
-      const determinant = this.coefficientMatrix.determinant;
+	#methods = {
+		cramer: () => {
+			const solution = {};
+			const determinant = this.coefficientMatrix.determinant;
 
-      this.variables.forEach((variable, index) => {
-        const replacedMatrix = this.coefficientMatrix.replaceCol(
-          index, this.constTerms
-        );
+			this.variables.forEach((variable, index) => {
+				const replacedMatrix = this.coefficientMatrix.replaceCol(
+					index, this.constTerms
+				);
 
-        solution[variable] = replacedMatrix.determinant / determinant;
-      });
+				solution[variable] = replacedMatrix.determinant / determinant;
+			});
 
-      return solution;
-    },
+			return solution;
+		},
 
-    matrix: () => {
-      const solution = {};
+		matrix: () => {
+			const solution = {};
 
-      return solution;
-    },
-  }
+			return solution;
+		},
+	}
 
-  constructor(linearEquations) {
-    const polynomRe = /((?:\+|\-)?\d+(?:\.\d+)?)([a-z])/g;
+	constructor(linearEquations) {
+		const polynomRe = /((?:\+|\-)?\d+(?:\.\d+)?)([a-z])/g;
 
-    this.coefficientMatrix = new Matrix(linearEquations.map(linearEquation => {
-      return [...linearEquation.matchAll(polynomRe)].map(match => {
-        return Number(match[1]);
-      });
-    }));
+		this.coefficientMatrix = new Matrix(linearEquations.map(linearEquation => {
+			return [...linearEquation.matchAll(polynomRe)].map(match => {
+				return Number(match[1]);
+			});
+		}));
 
-    this.variables = [...linearEquations[0].matchAll(polynomRe)].map(match => {
-      return match[2];
-    });
+		this.variables = [...linearEquations[0].matchAll(polynomRe)].map(match => {
+			return match[2];
+		});
 
-    this.constTerms = [...linearEquations.join(' ').matchAll(
-      /=(\d+(?:\.\d+)?)/g
-    )].map(match => Number(match[1]));
-  }
+		this.constTerms = [...linearEquations.join(' ').matchAll(
+			/=(\d+(?:\.\d+)?)/g
+		)].map(match => Number(match[1]));
+	}
 
-  solve(method = LinearSystem.methods.matrix) {
-    switch (method) {
-      case LinearSystem.methods.matrix:
-        return this.#methods.matrix();
-      case LinearSystem.methods.cramer:
-        return this.#methods.cramer();
-      default:
-        console.error('No such method');
-    }
-  }
+	solve(method = LinearSystem.methods.matrix) {
+		switch (method) {
+			case LinearSystem.methods.matrix:
+				return this.#methods.matrix();
+			case LinearSystem.methods.cramer:
+				return this.#methods.cramer();
+			default:
+				console.error('No such method');
+		}
+	}
 }
 
 const linearSystem = new LinearSystem([
-  '1.24x-0.87y-3.17z=0.46',
-  '2.11x-0.45y+1.44z=1.5',
-  '0.48x+1.25y-0.63z=0.35',
+	'1.24x-0.87y-3.17z=0.46',
+	'2.11x-0.45y+1.44z=1.5',
+	'0.48x+1.25y-0.63z=0.35',
 ]);
 
 console.log(linearSystem.coefficientMatrix.determinant);
